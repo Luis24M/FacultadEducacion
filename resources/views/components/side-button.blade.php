bb7e240f29d3810512c0cb9e65678a8e85fadaa4
Luis Morales <79761378+Luis24M@users.noreply.github.com>
2024-09-26 20:16:01 +0000

    cambiar iconos

--- resources/views/components/side-button.blade.php
+++ resources/views/components/side-button.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ $route }}" class="mx-2 hover:bg-black hover:text-white transition-all duration-300  flex text-center border border-black rounded-md p-2 bg-[#eee] shadow-lg my-4">
-    <img class="w-7 h-7" src="{{ $icon }}" alt=""> 
+<i class="{{ $icon }}"></i>
     <p class="text-center w-full ">
         {{ $title }}
     </p>
--- resources/views/components/side-menu.blade.php
+++ resources/views/components/side-menu.blade.php
@@ -1,13 +1,13 @@
 @if(request()->routeIs('home'))
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Institucional" route="/institucional" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Pregrado" route="/pregrado" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Posgrado" route="/posgrado" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Unidades" route="/unidades" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Departamentos academicos"
+    <x-side-button icon="fa-solid fa-house" title="Institucional" route="/institucional" />
+    <x-side-button icon="fa-solid fa-house" title="Pregrado" route="/pregrado" />
+    <x-side-button icon="fa-solid fa-house" title="Posgrado" route="/posgrado" />
+    <x-side-button icon="fa-solid fa-house" title="Unidades" route="/unidades" />
+    <x-side-button icon="fa-solid fa-house" title="Departamentos academicos"
         route="/departamentos-academicos" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Segunda especialidad" route="/segunda-especialidad" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="Cidunt" route="/cidunt" />
-    <x-side-button icon="{{ asset('icons/inicio.png') }}" title="R.N.C" route="/rnc" />
+    <x-side-button icon="fa-solid fa-house" title="Segunda especialidad" route="/segunda-especialidad" />
+    <x-side-button icon="fa-solid fa-house" title="Cidunt" route="/cidunt" />
+    <x-side-button icon="fa-solid fa-house" title="R.N.C" route="/rnc" />
 @elseif(request()->routeIs('institucional'))
     
 @elseif(request()->routeIs('pregrado'))
