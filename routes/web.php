879d932c200a779355d9d74728e1682db88a7d97
Lui24 <gnu936@gmail.com>
2024-09-30 10:22:19 -0500

    rename index main to welcome

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 })->name('home');
 
 Route::get('/institucional', function () {
