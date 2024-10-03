886c137e04e8d598fdb7ce5021e1d2e61af419f2
EcoSiccha <143729830+EcoSiccha@users.noreply.github.com>
2024-10-03 22:22:33 +0000

    agregar ruta

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,6 @@ Route::get('/pregrado/cdlc', function () {
 
 
 // Rutas Unidades
-<<<<<<< HEAD
 Route::get('/unidades/unidad_calidad', function () {
     return view('unidades.unidad_calidad.index');
 })->name('unidades.unidad_calidad');
@@ -86,11 +85,9 @@ Route::get('/unidades/unidad_calidad/auditoria_interna', function () {
 Route::get('/unidades/unidad_calidad/iso_9001', function () {
     return view('unidades.unidad_calidad.iso_9001');
 })->name('unidades.iso_9001');
-=======
 Route::get('/practica_preprofesional', function (){
     return view('unidades.practica_preprofesional.index');
 })->name('practica_preprofesional');
->>>>>>> 8f9dcef (avance_practica_preprofesional)
 
 Route::get('/unidades/unidad_calidad/acreditacion', function () {
     return view('unidades.unidad_calidad.acreditacion');
