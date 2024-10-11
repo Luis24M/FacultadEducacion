b4c72c3f09b49bb904b5e98f4d09abb8b1fa3424
renzoVilchez <156023308+renzoVilchez@users.noreply.github.com>
2024-10-11 22:33:58 +0000

    ciencias de la comunicacion

--- app/View/Components/dropDownButton.php
+++ app/View/Components/dropDownButton.php
@@ -1,39 +1,22 @@
 <?php
 
-<<<<<<< HEAD
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-class dropDownButton extends Component
-=======
+
 class DropDownButton extends Component
->>>>>>> 75cf832 (institucional y reseña de ciencias de la comunicacion)
 {
     public $title;
-    public $urlElemento;
-    public $urlSubelemento;
     public $list;
 
-    public function __construct($title, $urlElemento,$urlSubelemento , $list)
+    public function __construct($title, $list)
     {
         $this->title = $title;
-<<<<<<< HEAD
-=======
-        $this->urlElemento = $urlElemento;
-        $this->urlSubelemento = $urlSubelemento;
->>>>>>> 75cf832 (institucional y reseña de ciencias de la comunicacion)
         $this->list = $list;
     }
 
-    public function render(): View|Closure|string
+    public function render()
     {
         return view('components.drop-down-button');
     }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> 75cf832 (institucional y reseña de ciencias de la comunicacion)
--- resources/views/components/drop-down-button.blade.php
+++ resources/views/components/drop-down-button.blade.php
@@ -1,24 +1,18 @@
-<!-- resources/views/components/side-button.blade.php -->
 <div class="mx-2 my-4 dropdown">
-    @foreach ($temas as $tema)
-        <div class="hover:bg-black hover:text-white transition-all duration-300 flex text-center border border-black rounded-md p-2 bg-[#eee] shadow-lg">
-            <div class="flex w-full">
-                <button type="button" class="flex justify-center items-center w-full dropdown-button" aria-expanded="false" aria-haspopup="true" data-dropdown>
-                    {{ $tema['nombre'] }}
-                    <svg class="ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                </button>
-            </div>
-        </div>
+    <div class="hover:bg-black hover:text-white transition-all duration-300 flex text-center border border-black rounded-md p-2 bg-[#eee] shadow-lg">
+        <button type="button" class="flex justify-center items-center w-full dropdown-button" aria-expanded="false" aria-haspopup="true" data-dropdown>
+            {{ $title }}
+            <svg class="ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
 
-        <div class="mt-2 w-full rounded-md shadow-lg bg-[#eee] ring-1 ring-black ring-opacity-5 max-h-0 overflow-hidden transition-all duration-300 ease-in-out dropdown-menu" role="menu" aria-orientation="vertical" tabindex="-1" data-dropdown-menu>
-            <div class="py-1" role="none">
-                @foreach ($tema['subtemas'] as $subtema)
-                    <a href="{{ $subtema['urlSubelemento'] }}" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem">{{ $subtema['name'] }}</a>
-                @endforeach
-            </div>
+    <div class="mt-2 w-full rounded-md shadow-lg bg-[#eee] ring-1 ring-black ring-opacity-5 max-h-0 overflow-hidden transition-all duration-300 ease-in-out dropdown-menu" role="menu" aria-orientation="vertical" tabindex="-1" data-dropdown-menu>
+        <div class="py-1" role="none">
+            @foreach ($list as $item)
+                <a href="{{ $item['url'] }}" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem">{{ $item['name'] }}</a>
+            @endforeach
         </div>
     </div>
-    @endforeach
 </div>
