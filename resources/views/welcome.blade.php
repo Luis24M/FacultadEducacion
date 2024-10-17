bf90cde82e286fb1272b9a6f7cb6071817d1cdbd
Lui24 <gnu936@gmail.com>
2024-10-16 22:52:41 -0500

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,122 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2 class="text-5xl text-center my-8 font-bold">Nuestras Escuelas</h2>
-    <section class="p-4 flex lg:flex-row flex-col flex-wrap justify-around gap-4">
-        <x-card-escuelas name="Educación Inicial" url="/pregrado/inicial" img="{{ asset('imagenes/index/inicial.jpg') }}" />
-        <x-card-escuelas name="Educación Primaria" url="/pregrado/primaria" img="{{ asset('imagenes/index/primaria.jpg') }}" />
-        <x-card-escuelas name="Educación Secundaria" url="/pregrado/secundaria"
-            img="{{ asset('imagenes/index/secundaria.jpg') }}" />
-        <x-card-escuelas name="Ciencias de la Comunicación" url="/pregrado/cdlc"
-            img="{{ asset('imagenes/index/ciencias.jpg') }}" />
-    </section>
-
-    <h2 class="text-5xl text-center my-8 font-bold">Eventos</h2>
-    <div class="flex flex-col gap-5 items-center mx-auto lg:flex-row justify-around p-4">
-        <img src="{{ asset('imagenes/index/auditorio.jpg') }}" class="rounded float lg:w-[40%] shadow-lg" alt="...">
-        <img src="{{ asset('imagenes/index/juegos.jpg') }}" class="rounded float lg:w-[30%] shadow-lg" alt="...">
+    <div class="my-10">
+        <h2 class="text-5xl text-center my-8 font-bold">Noticias</h2>
+        <section class="flex lg:flex-row flex-col p-4 gap-4 justify-around mx-auto max-w-7xl">
+            <x-card-noticias titulo="Título de la noticia" fecha="09 Octubre 2024"
+                imagen="{{ asset('imagenes/index/noticia1.png') }}" descripcion="Descripcion de prueba" />
+            <x-card-noticias titulo="Título de la noticia" fecha="09 Octubre 2024"
+                imagen="{{ asset('imagenes/index/noticia1.png') }}" descripcion="Descripcion de prueba" />
+            <x-card-noticias titulo="Título de la noticia" fecha="09 Octubre 2024"
+                imagen="{{ asset('imagenes/index/noticia1.png') }}" descripcion="Descripcion de prueba" />
+        </section>
+        <div class="w-full text-center py-4">
+            <a href="/noticias" id="noticias" class="text-xl bg-neutral-950 text-white p-3 hover:shadow-2xl">
+                <span>
+                    Ver todas las noticias
+                </span>
+            </a>
+        </div>
+    </div>
+    <hr>
+    <div class="my-10">
+        <h2 class="text-5xl text-center my-8 font-bold">Eventos</h2>
+        <div class="flex flex-col gap-5 items-center mx-auto lg:flex-row justify-around p-4">
+            <img src="{{ asset('imagenes/index/auditorio.jpg') }}" class="rounded float lg:w-[40%] shadow-lg"
+                alt="...">
+            <img src="{{ asset('imagenes/index/juegos.jpg') }}" class="rounded float lg:w-[30%] shadow-lg" alt="...">
+        </div>
+    </div>
+    <hr>
+    <div class="flex flex-col lg:flex-row p-10 w-full justify-center my-10">
+        <div>
+            <img class="min-w-[30%]" src="{{ asset('imagenes/index/decana.png') }}"
+                alt="decana Elizabeth Aurea Rafael Sánchez">
+        </div>
+        <div class="border-2 bg-white p-4 lg:p-20 lg:max-w-[50%] rounded-md text-center">
+            <h3 class="text-3xl font-semibold">Bienvenida</h3>
+            <br>
+            <p>"Bienvenidos a la Facultad de Educación y Ciencias de la Comunicación, un espacio donde la excelencia
+                académica se encuentra con la innovación y la pasión por el aprendizaje. En nuestra facultad, nos
+                enorgullece nutrir no solo las mentes brillantes de hoy, sino también los líderes del mañana. Estamos
+                comprometidos con la creación de un ambiente educativo que fomente la curiosidad, la creatividad y el
+                pensamiento crítico. Aquí, cada estudiante es más que un número; son parte de una comunidad académica
+                diversa y vibrante. Con un cuerpo docente dedicado y programas de vanguardia, aspiramos a impulsar el
+                crecimiento intelectual y personal de cada individuo que cruza nuestras puertas. Juntos, construiremos un
+                futuro donde el conocimiento se traduzca en impacto, y donde cada logro sea una prueba de la dedicación y el
+                compromiso de nuestros estudiantes y educadores.
+                ¡Bienvenidos a una experiencia educativa que va más allá de las aulas y trasciende fronteras!"
+                <br>
+                <br>
+                Elizabeth Aurea Rafael Sánchez
+                <br>
+                Decana
+            </p>
+        </div>
+    </div>
+    <hr>
+    <div class="my-10">
+        <h2 class="text-5xl text-center my-8 font-bold">Nuestras Escuelas</h2>
+        <section class="p-4 flex lg:flex-row flex-col flex-wrap justify-around gap-4 lg:gap-1">
+            <x-card-escuelas name="Educación Inicial" url="/pregrado/inicial"
+                img="{{ asset('imagenes/index/inicial.jpg') }}" />
+            <x-card-escuelas name="Educación Primaria" url="/pregrado/primaria"
+                img="{{ asset('imagenes/index/primaria.jpg') }}" />
+            <x-card-escuelas name="Educación Secundaria" url="/pregrado/secundaria"
+                img="{{ asset('imagenes/index/secundaria.jpg') }}" />
+            <x-card-escuelas name="Ciencias de la Comunicación" url="/pregrado/cdlc"
+                img="{{ asset('imagenes/index/ciencias.jpg') }}" />
+        </section>
     </div>
 
-    <h2 class="text-5xl text-center my-8 font-bold">Noticias</h2>
-    <section class="flex lg:flex-row flex-col p-4 gap-4 justify-around mx-auto max-w-7xl">
-        <x-card-noticias titulo="Título de la noticia" fecha="09 Octubre 2024"
-            imagen="{{ asset('imagenes/index/noticia1.png') }}" descripcion="Descripcion de prueba" />
-        <x-card-noticias titulo="Título de la noticia" fecha="09 Octubre 2024"
-            imagen="{{ asset('imagenes/index/noticia1.png') }}" descripcion="Descripcion de prueba" />
-        <x-card-noticias titulo="Título de la noticia" fecha="09 Octubre 2024"
-            imagen="{{ asset('imagenes/index/noticia1.png') }}" descripcion="Descripcion de prueba" />
-    </section>
-    <div class="w-full text-center py-4">
-        <a href="/noticias" class="text-xl bg-neutral-950 text-white p-3 hover:shadow-2xl">Ver todas las noticias</a>
-    </div>
+    <style>
+        #noticias {
+            position: relative;
+            display: flex;
+            max-width: 400px;
+            margin: 0 auto;
+            justify-content: center;
+            align-items: center;
+            border-radius: 5px;
+            background: #183153;
+            box-shadow: 0px 6px 24px 0px rgba(0, 0, 0, 0.2);
+            overflow: hidden;
+            cursor: pointer;
+            border: none;
+        }
+
+        #noticias:after {
+            content: " ";
+            width: 0%;
+            height: 100%;
+            background: #ffd401;
+            position: absolute;
+            transition: all 0.4s ease-in-out;
+            right: 0;
+        }
+
+        #noticias:hover::after {
+            right: auto;
+            left: 0;
+            width: 100%;
+        }
+
+        #noticias span {
+            text-align: center;
+            text-decoration: none;
+            width: 100%;
+            padding: 12px 20px;
+            color: #fff;
+            font-size: 1.125em;
+            font-weight: 600;
+            z-index: 20;
+            transition: all 0.3s ease-in-out;
+        }
+
+        #noticias:hover span {
+            color: #183153;
+            animation: scaleUp 0.3s ease-in-out;
+        }
+    </style>
 @endsection
