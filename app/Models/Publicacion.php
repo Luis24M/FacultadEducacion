611a6f2346be264fd60decc58f816a48a0c9bd21
Pauljls <paulazarsol@gmail.com>
2024-12-13 10:22:02 -0500

    correcion rsu

--- app/Models/Publicacion.php
+++ app/Models/Publicacion.php
@@ -3,33 +3,56 @@
 /**
  * Created by Reliese Model.
  */
+
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User; // Importa el modelo User
 
+/**
+ * Class Publicacion
+ * 
+ * @property int $idPublicacion
+ * @property string|null $nombrePublicacion
+ * @property string|null $desPublicacion
+ * @property Carbon|null $fechaPublicacion
+ * @property string|null $imgPublicacion
+ * @property int|null $idUsuario
+ * @property int|null $idTipoPublicacion
+ * 
+ * @property Usuario|null $usuario
+ * @property Tipopublicacion|null $tipopublicacion
+ *
+ * @package App\Models
+ */
 class Publicacion extends Model
 {
-    protected $table = 'publicacion'; // Tu tabla personalizada
-    protected $primaryKey = 'idPublicacion'; // Clave primaria
-    public $timestamps = false;
+	protected $table = 'publicacion';
+	protected $primaryKey = 'idPublicacion';
+	public $timestamps = false;
 
-    protected $fillable = [
-        'nombrePublicacion',
-        'desPublicacion',
-        'fechaPublicacion',
-        'imgPublicacion',
-        'idUsuario', 
-        'idTipoPublicacion'
-    ];
+	protected $casts = [
+		'fechaPublicacion' => 'datetime',
+		'idUsuario' => 'int',
+		'idTipoPublicacion' => 'int'
+	];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'idUsuario', 'id'); // Relación con el modelo User
-    }
+	protected $fillable = [
+		'nombrePublicacion',
+		'desPublicacion',
+		'fechaPublicacion',
+		'imgPublicacion',
+		'idUsuario',
+		'idTipoPublicacion'
+	];
 
-    public function tipopublicacion()
-    {
-        return $this->belongsTo(Tipopublicacion::class, 'idTipoPublicacion');
-    }
+	public function usuario()
+	{
+		return $this->belongsTo(Usuario::class, 'idUsuario');
+	}
+
+	public function tipopublicacion()
+	{
+		return $this->belongsTo(Tipopublicacion::class, 'idTipoPublicacion');
+	}
 }
